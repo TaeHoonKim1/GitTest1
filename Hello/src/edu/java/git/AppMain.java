c2585071f78571487f8298a2ee474a98fe8ef130
TaeHoonKim1 <rlaxognsa@naver.com>
2016-11-11 14:17:36 +0900

    Update AppMain.java

--- Hello/src/edu/java/git/AppMain.java
+++ Hello/src/edu/java/git/AppMain.java
@@ -4,8 +4,8 @@ public class AppMain {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		System.out.println("�ȳ��ϼ���~");
-
+		System.out.println("¾È³çÇÏ¼¼¿ä~");
+        System.out.println("hi");
 	}
 
 }
