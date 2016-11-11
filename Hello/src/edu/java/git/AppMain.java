d59d919ef1ec79242ee61da164bdc0ed9470a8f9
TaeHoonKim1 <rlaxognsa@naver.com>
2016-11-11 14:49:46 +0900

    홍길동 고침

--- Hello/src/edu/java/git/AppMain.java
+++ Hello/src/edu/java/git/AppMain.java
@@ -3,7 +3,7 @@ package edu.java.git;
 public class AppMain {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello Git");
 		System.out.println("¾È³çÇÏ¼¼¿ä~");
         System.out.println("hi");
 	}
